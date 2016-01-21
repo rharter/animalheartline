bfc6f03de4088beb9f99d7cbbe7ae341d9d0184c
Ryan Harter <ryanjharter@gmail.com>
2016-01-21 10:29:46 -0600

    Working Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,52 @@
 module.exports = function(grunt) {
 
-  // grunt.initConfig({
-  //   pkg: grunt.file.readJSON('package.json'),
-  //   coffee: {
-  //     app: {
-  //       src: ["coffee/app.coffee"],
-  //       dest: "js/",
-  //       options: {
-  //         bare: true
-  //       }
-  //     }
-  //   },
-  //   compass: {
-  //     dist: {
-  //       options: {
-  //         sassDir: 'sass',
-  //         cssDir: 'assets/',
-  //         imagesDir: '/img',
-  //         relativeAssets: true,
-  //         noLineComments: true,
-  //         environment: 'production'
-  //       }
-  //     },
-  //     dev: {
-  //       options: {
-  //         sassDir: 'sass',
-  //         cssDir: 'assets/',
-  //         imagesDir: '/img',
-  //         relativeAssets: true
-  //       }
-  //     }
-  //   },
-  //   watch: {
-  //     coffee: {
-  //       files: ["<config:coffee.app.src>"],
-  //       tasks: "js"
-  //     },
-  //     jslibs: {
-  //       files: ["_jslibs/*.js"],
-  //       tasks: "js"
-  //     },
-  //     sass: {
-  //       files: ["style/*"],
-  //       tasks: "compass"
-  //     }
-  //   }
-  // });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      app: {
+        src: ["coffee/app.coffee"],
+        dest: "dist/js/app.js",
+        options: {
+          bare: true
+        }
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'dist/assets/',
+          imagesDir: 'dist/img',
+          relativeAssets: true,
+          noLineComments: true,
+          environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'dist/assets/',
+          imagesDir: 'dist/img',
+          relativeAssets: true
+        }
+      }
+    },
+    watch: {
+      jslibs: {
+        files: ["_jslibs/*.js"],
+        tasks: "js"
+      },
+      sass: {
+        files: ["style/*"],
+        tasks: "compass"
+      }
+    }
+  });
 
   // https://github.com/gruntjs/grunt-contrib-coffee
-  grunt.loadNpmTask('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   // https://github.com/gruntjs/grunt-contrib-compass
-  grunt.loadNpmTask('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
   grunt.registerTask('default', ['compass', 'js']);
   grunt.registerTask('js', 'coffee');
